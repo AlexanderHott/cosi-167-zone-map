b1246088d548ba19c0dc5089a479e70b3c954b14
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2024-09-06 14:59:43 -0400

    format

--- zonemap.cpp
+++ zonemap.cpp
@@ -1,16 +1,13 @@
 #include "zonemap.h"
 
 template<typename T>
-zonemap<T>::zonemap(std::vector<T> _elements, uint _num_elements_per_zone)
-{
-    // constructor 
+zonemap<T>::zonemap(std::vector<T> _elements, uint _num_elements_per_zone) {
+    // constructor
     // Your code starts here ...
 }
 
 template<typename T>
-size_t zonemap<T>::query(T key)
-{
+size_t zonemap<T>::query(T key) {
     // Your code starts here ...
     return 0;
 }
-
